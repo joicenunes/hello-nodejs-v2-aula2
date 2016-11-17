f7e4336953b77027dd63be6d6a107e305b65bfdb
joicenunes <anunes.joice@gmail.com>
2016-11-17 09:57:14 -0300

    Create hello3.js

---
+++ 2.1-introducao-nodejs/hello3.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Digite um número\n', (num) => {
+  if(isNaN(num))
+    console.log(`${num} não é um número válido`);
+  else
+    console.log(`Você digitou o número ${num}`);
+  rl.close();
+});
